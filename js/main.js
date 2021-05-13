7beb4038d4b2d03f99dbf5d09de12b7455ccd05a
elid venega <venegaelid@gmail.com>
2021-05-13 06:46:12 -0700

    Work on nav making sure you can click links while on mobile

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_links');
-    const navLinks = document.querySelectorAll('.nav_links li')
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav_links');
+const navLinks = document.querySelectorAll('.nav_links li')
+
+const navSlide = function () {
     burger.addEventListener('click', () => {
         // Toggle nav
         nav.classList.toggle('nav-active');
